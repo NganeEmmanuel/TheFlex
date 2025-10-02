59a7a7f47ef8533e8f7542c6dc057be387e78037
Ngane Emmanuel <emmanuelngane06@gmail.com>
2025-10-02 13:38:38 -0700

    adding vercel configuration to backend

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import reviewRoutes from "../routes/reviews.js";
 
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -10,5 +9,4 @@ app.use(express.json());
 // Routes
 app.use("/api/reviews", reviewRoutes);
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
+export default app;
